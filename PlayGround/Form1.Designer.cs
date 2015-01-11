15f19d88cf8e869e35abd786133f860139cbdf5a
SharkSpeed <ijsantiagoiii@gmail.com>
2015-01-11 12:43:06 +0800

    moved to left added button

--- PlayGround/Form1.Designer.cs
+++ PlayGround/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.c1Calendar1 = new C1.Win.C1Schedule.C1Calendar();
             this.c1QRCode1 = new C1.Win.C1BarCode.C1QRCode();
+            this.c1Button1 = new C1.Win.C1Input.C1Button();
             ((System.ComponentModel.ISupportInitialize)(this.c1Calendar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,24 +52,34 @@
             System.DayOfWeek.Wednesday,
             System.DayOfWeek.Thursday,
             System.DayOfWeek.Friday});
-            this.c1Calendar1.Location = new System.Drawing.Point(316, 84);
+            this.c1Calendar1.Location = new System.Drawing.Point(12, 84);
             this.c1Calendar1.Name = "c1Calendar1";
             this.c1Calendar1.Size = new System.Drawing.Size(181, 142);
             this.c1Calendar1.TabIndex = 0;
             // 
             // c1QRCode1
             // 
-            this.c1QRCode1.Location = new System.Drawing.Point(361, 12);
+            this.c1QRCode1.Location = new System.Drawing.Point(12, 12);
             this.c1QRCode1.Name = "c1QRCode1";
             this.c1QRCode1.Size = new System.Drawing.Size(66, 66);
             this.c1QRCode1.TabIndex = 1;
             this.c1QRCode1.Text = "c1QRCode1";
+            // 
+            // c1Button1
+            // 
+            this.c1Button1.Location = new System.Drawing.Point(97, 48);
+            this.c1Button1.Name = "c1Button1";
+            this.c1Button1.Size = new System.Drawing.Size(75, 23);
+            this.c1Button1.TabIndex = 2;
+            this.c1Button1.Text = "c1Button1";
+            this.c1Button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(885, 443);
+            this.Controls.Add(this.c1Button1);
             this.Controls.Add(this.c1QRCode1);
             this.Controls.Add(this.c1Calendar1);
             this.Name = "Form1";
@@ -82,6 +93,7 @@
 
         private C1.Win.C1Schedule.C1Calendar c1Calendar1;
         private C1.Win.C1BarCode.C1QRCode c1QRCode1;
+        private C1.Win.C1Input.C1Button c1Button1;
     }
 }
 
